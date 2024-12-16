87564635f6b976deb0a4acfbe3d7f39e0b68f86b
jeremy-lim-dh <jeremy.lim@foodpanda.com>
2024-12-16 18:04:55 +0800

    Create 4_products.sql

---
+++ db/4_products.sql
@@ -0,0 +1,10 @@
+USE mydb;
+
+INSERT INTO products (name, description, price, discount, baseImage, availability, supermarketID)
+VALUES
+('Vegetables rescue box (contains 12 different items)', 'The Rescue Box is an eco-friendly option designed to reduce food waste by making use of vegetables...', 20.70, 0.51, 'https://res.cloudinary.com/dkgnjflxq/image/upload/v1734016496/pandamart-rescue1_jsknhb.png', 1, 1),
+('Fruits rescue box (contains 12 different items)', 'The Rescue Box is an eco-friendly option designed to reduce food waste by rescuing fruits...', 25.80, 0.54, 'https://res.cloudinary.com/dkgnjflxq/image/upload/v1734016493/pandamart-rescue2_eveffe.webp', 1, 1),
+('Canned food rescue box (contains 12 different items)', 'The Rescue Box is an eco-friendly option designed to reduce food waste by offering canned foods...', 34.10, 0.57, 'https://res.cloudinary.com/dkgnjflxq/image/upload/v1734016494/pandamart-rescue3_czphm6.jpg', 1, 1),
+('Freshstory King Tangerines 600g', 'China tangerine', 4.90, 0.29, 'https://images.deliveryhero.io/image/darkstores/sgp/Benelux_Flowers_Food_Pte_Ltd/14112023/6970601451140.jpg', 1, 1),
+('brightfarms Australian Cherries (Large Size) 500g', 'Bright and tangy, these fresh cherries...', 19.9, 0.20, 'https://images.deliveryhero.io/image/darkstores/sgp/Brightfarms/20240527_Frozen_Chilled/79Y649.jpg', 1, 1),
+('brightfarms Australia R2E2 Mango 500-600g', 'Indulge in the sweet splendor of R2E2 mangoes...', 6.9, 0.15, 'https://images.deliveryhero.io/image/darkstores/nv-global-catalog/sg/508a478f-88d5-440c-a578-343c330de330.png', 1, 1);
