d2646293e7e86ad35e91156b6338c742915c09ac
jeremy-lim-dh <jeremy.lim@foodpanda.com>
2024-12-16 18:04:45 +0800

    Create 3_supermarkets.sql

---
+++ db/3_supermarkets.sql
@@ -0,0 +1,5 @@
+USE mydb;
+
+INSERT INTO supermarkets (name, listing, logo, deliveryTime, deliveryFee, address, latitude, longitude)
+VALUES
+('pandamart (CBD)', 'https://images.deliveryhero.io/image/fd-sg/LH/cl5jl-listing.jpg', 'https://images.deliveryhero.io/image/fd-sg/LH/cl5jl-listing.jpg', '20-35 min', '0', '101-109 Telok Ayer St', 1.2817747393019985, 103.84843155739871);
